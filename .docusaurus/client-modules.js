402e99e1b0ff482110445321765ec77f797e0d9d
Rae Sharp <resharp20@gmail.com>
2025-07-13 21:58:11 -0400

    Revert "Remove .docusaurus folder"
    This reverts commit 0285e3ac1d4282857068498cc40fa4c6fb3382cf.

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/rae/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/rae/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/rae/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/rae/docs/src/css/custom.css"),
+];
