db4f87b2b5d21aad5b4cf3e77ae5a97cd65b9645
Rae Sharp <resharp20@gmail.com>
2026-03-23 19:04:34 -0400

    add kubecon banner
    Signed-off-by: Rae Sharp <resharp20@gmail.com>
    
    Signed-off-by: Rae Sharp <resharp20@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,14 +111,14 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            // announcementBar: {
-            //     id: "my_custom_banner",
-            //     content:
-            //         '<a href="https://www.upbound.io/events/kubecon-cloudnativecon-north-america-atlanta-2025" target="_blank" rel="noopener noreferrer">🎉 Join us at KubeCon Atlanta!</a>',
-            //     backgroundColor: "#9b5efc",
-            //     textColor: "#fff",
-            //     isCloseable: true,
-            // },
+             announcementBar: {
+                 id: "my_custom_banner",
+                 content:
+                     '<a href="https://www.upbound.io/events/kubecon-cloudnativecon-emea-amsterdam-2026" target="_blank" rel="noopener noreferrer">🎉 Visit us at KubeCon Amsterdam!</a>',
+                 backgroundColor: "#9b5efc",
+                 textColor: "#fff",
+                 isCloseable: true,
+             },
             head: [
                 [
                     "meta",
