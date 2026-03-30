a74a0efd503d6e81bcf3798d6319105d4ac697c2
rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>
2026-03-30 16:51:22 -0400

    Add askAi key to Algolia configuration (#1124)
    * Add askAi key to Algolia configuration
    
    Signed-off-by: rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>
    
    * Update Algolia configuration in docusaurus.config.js
    
    Signed-off-by: rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>
    
    * Change Algolia index name from 'upbound-docs' to 'upbound'
    
    Signed-off-by: rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>
    
    * Rename algoliaAskAiKey to askAi in config
    
    Signed-off-by: rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: rae sharp ♯ <8883519+tr0njavolta@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -212,11 +212,12 @@ const config = {
             },
             algolia: {
                 appId: "4OZX85VEXQ",
-                apiKey: "9f96702edcf79d7097bedfce2813d49e",
+                apiKey: process.env.ALGOLIA_API_KEY || '',
                 indexName: "upbound",
-                contextualSearch: true,
+                contextualSearch: true, 
                 searchPagePath: "search",
                 searchParameters: {},
+                askAi: process.env.ALGOLIA_ASK_AI_KEY || '',
             },
             footer: {
                 style: "dark",
