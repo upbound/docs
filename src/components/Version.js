21db549379cd8c9ae52e0346a5868b329e91e65c
Rae Sharp <resharp20@gmail.com>
2026-07-01 11:17:42 -0400

    Simplify interpolation

--- src/components/Version.js
+++ src/components/Version.js
@@ -1,14 +1,10 @@
 export const versions = {
-  cli: '0.48.2.',
+  cli: '0.48.2',
   spaces: '1.17.0',
   aws: '2.6.1',
   azure: '2.6.0',
   gcp: '2.6.0',
   helm: '1.3.0',
   kubernetes: '1.2.6',
-  mcpconnector: 'v0.10.0'
+  mcpconnector: '0.10.0'
 };
-
-export default function Version({type = 'cli'}) {
-  return <span>{versions[type]}</span>;
-}
